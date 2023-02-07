004b9f8c5cfb32f86e0af8dd4d6c186e18259fa8
kelvinkoon <kkoon@shaw.ca>
2023-02-07 02:27:17 -0500

    Add item parsing and ability parsing functionality

--- src/rk9_tour_analysis/parse_roster.py
+++ src/rk9_tour_analysis/parse_roster.py
@@ -43,13 +43,16 @@ def get_roster_info_df(soup: BeautifulSoup):
         player_info_df_entry["full_name"] = get_full_name(player_info_entry)
         player_info_df_entry["standing"] = get_standing(player_info_entry)
         # Team List is parsed with soup as the URI isn't part of text
-        for num, pkmn in enumerate(get_team_list(player_info_soup), 1):
-            player_info_df_entry["pkmn" + str(num)] = pkmn
+        for pkmn_num, pkmn_info in enumerate(get_team_list(player_info_soup), 1):
+            player_info_df_entry["pkmn" + str(pkmn_num)] = pkmn_info["species"]
+            player_info_df_entry["pkmn" + str(pkmn_num) + "_item"] = pkmn_info["held_item"]
+            player_info_df_entry["pkmn" + str(pkmn_num) + "_moves"] = pkmn_info["moves"]
         print(player_info_df_entry)
         player_info_df_entry_list.append(player_info_df_entry)
     # TODO: Make columns a constant
     df = pd.DataFrame(player_info_df_entry_list, columns=["full_name", "standing", 
-        "pkmn1", "pkmn2", "pkmn3", "pkmn4", "pkmn5", "pkmn6"])
+        "pkmn1", "pkmn1_item", "pkmn1_moves", "pkmn2", "pkmn2_item", "pkmn2_moves", "pkmn3", "pkmn3_item", "pkmn3_moves",
+          "pkmn4", "pkmn4_item", "pkmn4_moves", "pkmn5", "pkmn5_item", "pkmn5_moves", "pkmn6", "pkmn6_item", "pkmn6_moves"])
     return df
 
 def write_df_to_csv(df: pd.DataFrame):
@@ -62,15 +65,38 @@ def get_team_list(soup: BeautifulSoup):
     team_list_soup = get_team_list_soup(team_list_id)
     # Get English team list
     eng_team_list_soup = team_list_soup.find("div", attrs={"id": "lang-EN"})
-    # Separate Pokémon info by "img" tag
-    pkmn_info_soup_list = eng_team_list_soup.find_all("img")
+    pkmn_info_soup_list = eng_team_list_soup.find_all("div", {"class": "pokemon"})
     for pkmn_info_soup in pkmn_info_soup_list: 
-        # Get text after img tag
-        pkmn_txt = pkmn_info_soup.next_sibling
-        pkmn_txt = "".join(pkmn_txt.split())
-        pkmn_txt = re.sub('".*?"', '', pkmn_txt)
-        team_list.append(pkmn_txt)
+        pkmn_info = {}
+        pkmn_info["species"] = get_species(pkmn_info_soup)
+        pkmn_info["held_item"] = get_held_item(pkmn_info_soup)
+        pkmn_info["moves"] = "/".join(get_moves(pkmn_info_soup))
+        team_list.append(pkmn_info)
     return team_list
+
+def get_species(soup: BeautifulSoup):
+    img_soup = soup.find("img")
+    soup_next_element = img_soup.next_sibling
+    # Remove whitespace
+    pkmn_species = "".join(soup_next_element.split())
+    # Remove nickname if applicable
+    pkmn_species = re.sub('".*?"', '', pkmn_species)
+    # Add space back (eg. Flutter Mane)
+    pkmn_species = re.sub(r"(\\w)([A-Z])", r"\1 \2", pkmn_species)
+    return pkmn_species
+
+def get_held_item(soup: BeautifulSoup):
+    held_item_soup = soup.find(lambda tag:tag.name=="b" and "Held Item" in tag.text)
+    soup_next_element = held_item_soup.next_sibling
+    held_item = soup_next_element.text
+    return held_item
+
+def get_moves(soup: BeautifulSoup):
+    move_list = []
+    move_soup_list = soup.find_all("span", {"class": "badge"})
+    for move_soup in move_soup_list:
+        move_list.append(move_soup.text)
+    return move_list
 
 def get_full_name(player_info_entry: List[str]):
     first_name = player_info_entry[FIRST_NAME_COL_INDEX]
